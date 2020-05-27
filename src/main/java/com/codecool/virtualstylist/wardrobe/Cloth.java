424dfd55094b61a20f7b8be1e390d00883fc52b0
Magdalena Gola <turcza.magdalena@gmail.com>
2020-05-27 18:26:17 +0200

    Add getters ans setters to cloth entity fields

--- src/main/java/com/codecool/virtualstylist/wardrobe/Cloth.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/Cloth.java
@@ -46,5 +46,115 @@ class Cloth {
     @ManyToMany(mappedBy = "clothes",fetch = FetchType.LAZY)
     private List<Stylization> stylizations;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public BodyPart getBodyPart() {
+        return bodyPart;
+    }
+
+    public void setBodyPart(BodyPart bodyPart) {
+        this.bodyPart = bodyPart;
+    }
+
+    public ClothType getClothType() {
+        return clothType;
+    }
+
+    public void setClothType(ClothType clothType) {
+        this.clothType = clothType;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Style getStyle() {
+        return style;
+    }
+
+    public void setStyle(Style style) {
+        this.style = style;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public boolean isHasPattern() {
+        return hasPattern;
+    }
+
+    public void setHasPattern(boolean hasPattern) {
+        this.hasPattern = hasPattern;
+    }
+
+    public String getShopLink() {
+        return shopLink;
+    }
+
+    public void setShopLink(String shopLink) {
+        this.shopLink = shopLink;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Stylization> getStylizations() {
+        return stylizations;
+    }
+
+    public void setStylizations(List<Stylization> stylizations) {
+        this.stylizations = stylizations;
+    }
 }
