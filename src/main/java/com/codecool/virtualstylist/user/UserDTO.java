7092e387fcd8495f4cb569d7538e2cdeb785898c
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-27 12:52:12 +0200

    Add data transfer object for user entity

---
+++ src/main/java/com/codecool/virtualstylist/user/UserDTO.java
@@ -0,0 +1,57 @@
+package com.codecool.virtualstylist.user;
+
+public class UserDTO {
+    private Integer id;
+    private String email;
+    private String password;
+    private String name;
+    private Gender gender;
+
+    public UserDTO(Integer id, String email, String password, String name, Gender gender) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
