a5abb3680d4a07c9c1c9c8f21b26e26899fe5870
Magdalena Gola <turcza.magdalena@gmail.com>
2020-05-27 11:03:24 +0200

    Initial commit

---
+++ src/main/java/com/codecool/virtualstylist/VirtualStylistApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.virtualstylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualStylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualStylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/virtualstylist/VirtualStylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.virtualstylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualStylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
