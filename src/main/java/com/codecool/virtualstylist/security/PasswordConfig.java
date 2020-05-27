21b21a8d4d8260e74b81a0f7653a1d4603dc5dd4
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-27 15:41:17 +0200

    Add password encoder configuration

---
+++ src/main/java/com/codecool/virtualstylist/security/PasswordConfig.java
@@ -0,0 +1,12 @@
+package com.codecool.virtualstylist.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+}
