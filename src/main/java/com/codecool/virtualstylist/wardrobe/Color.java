961daa487e10b256a9bc137fd42417eca52a3844
Magdalena Gola <turcza.magdalena@gmail.com>
2020-05-27 14:29:39 +0200

    Add options of clothes' colors

---
+++ src/main/java/com/codecool/virtualstylist/wardrobe/Color.java
@@ -0,0 +1,17 @@
+package com.codecool.virtualstylist.wardrobe;
+
+public enum Color {
+    WHITE,
+    BLACK,
+    RED,
+    BLUE,
+    YELLOW,
+    BROWN,
+    GREY,
+    GREEN,
+    ORANGE,
+    PINK,
+    GOLD,
+    SILVER,
+    PURPLE
+}
