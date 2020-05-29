86ceb6081a72a1c7459eb38100b76e4e0e0c0cbf
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-29 16:04:46 +0200

    Add deletebyclothidanduser method

--- src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeDataAccess.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeDataAccess.java
@@ -1,7 +1,7 @@
 package com.codecool.virtualstylist.wardrobe;
 
+import com.codecool.virtualstylist.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository("wardrobeDataAccess")
 interface WardrobeDataAccess extends JpaRepository<Cloth,Integer> {
     List<Cloth> findAllByUser_Id(int userId);
+    void deleteClothByIdAndUser(int clothId, User user);
 }
