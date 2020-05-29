85eae76b4260c3e7c0723544632282f0edc0ee2a
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-29 16:05:37 +0200

    Add cloth for display dto

---
+++ src/main/java/com/codecool/virtualstylist/wardrobe/ClothForDisplayWardrobeDTO.java
@@ -0,0 +1,17 @@
+package com.codecool.virtualstylist.wardrobe;
+
+class ClothForDisplayWardrobeDTO {
+    Integer id;
+
+    public ClothForDisplayWardrobeDTO(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
