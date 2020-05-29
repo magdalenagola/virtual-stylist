4d3f32541fd5e3a9ecfc9a43580af1a0fb5eff19
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-29 16:04:18 +0200

    Add get all edit and delete methods

--- src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeController.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeController.java
@@ -1,19 +1,37 @@
 package com.codecool.virtualstylist.wardrobe;
 
+import com.codecool.virtualstylist.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/wardrobe")
 public class WardrobeController {
 
     private WardrobeService wardrobeService;
-    private UserService userService;
+   // private UserService userService;
 
     @Autowired
-    public WardrobeController(WardrobeService wardrobeService, UserService userService) {
+    public WardrobeController(WardrobeService wardrobeService){//, UserService userService) {
         this.wardrobeService = wardrobeService;
-        this.userService = userService;
+       // this.userService = userService;
+    }
+
+    @GetMapping
+    public List<ClothForDisplayWardrobeDTO> getAllClothes(){
+        return wardrobeService.getAllClothesByUserId(1);
+    }
+
+    @PutMapping
+    public void editCloth(@RequestBody ClothForUpdateDTO cloth){
+        wardrobeService.editCloth(cloth);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCloth(@PathVariable("id") Integer id){
+        //TODO findUser
+        wardrobeService.deleteCloth(id, new User());
     }
 }
