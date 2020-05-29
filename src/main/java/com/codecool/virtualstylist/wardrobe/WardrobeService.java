cc330c1afa22018fa3225203875e5ffcceb1e4d8
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-29 16:05:13 +0200

    Update get edit and delete methods

--- src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeService.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeService.java
@@ -1,38 +1,48 @@
 package com.codecool.virtualstylist.wardrobe;
 
+
+import com.codecool.virtualstylist.user.User;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 class WardrobeService {
     private WardrobeDataAccess wardrobeDataAccess;
+    private ModelMapper modelMapper;
 
     @Autowired
-    public WardrobeService(@Qualifier("wardrobeDataAccess") WardrobeDataAccess wardrobeDataAccess) {
+    public WardrobeService(@Qualifier("wardrobeDataAccess") WardrobeDataAccess wardrobeDataAccess, ModelMapper modelMapper) {
         this.wardrobeDataAccess = wardrobeDataAccess;
+        this.modelMapper = modelMapper;
     }
 
     void addCloth(Cloth cloth){
         wardrobeDataAccess.save(cloth);
     }
 
-    void editCloth(Cloth cloth){
+    void editCloth(ClothForUpdateDTO clothToUpdate){
+        Cloth cloth = modelMapper.map(clothToUpdate, Cloth.class);
         wardrobeDataAccess.save(cloth);
     }
 
-    void deleteCloth(Cloth cloth){
-        wardrobeDataAccess.delete(cloth);
+    void deleteCloth(Integer id, User user){
+        wardrobeDataAccess.deleteClothByIdAndUser(id, user);
     }
 
     Optional<Cloth> getClothById(int clothId){
         return wardrobeDataAccess.findById(clothId);
     }
 
-    List<Cloth> getAllClothesByUserId(int userId){
-        return wardrobeDataAccess.findAllByUser_Id(userId);
+    List<ClothForDisplayWardrobeDTO> getAllClothesByUserId(int userId){
+        List<Cloth> clothes =  wardrobeDataAccess.findAllByUser_Id(userId);
+        return clothes.stream()
+                .map(cloth -> modelMapper.map(cloth,ClothForDisplayWardrobeDTO.class))
+                .collect(Collectors.toList());
     }
 }
