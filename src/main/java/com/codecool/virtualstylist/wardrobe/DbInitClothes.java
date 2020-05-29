ce4c1063c0b664063b9fe75498d9bfa13d5b808b
szczepanTopolski <szczepanPL678@gmail.com>
2020-05-29 17:50:59 +0200

    Extract clothes initialization from user

--- src/main/java/com/codecool/virtualstylist/wardrobe/DbInitClothes.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/DbInitClothes.java
@@ -1,30 +1,25 @@
-package com.codecool.virtualstylist.user;
+package com.codecool.virtualstylist.wardrobe;
 
-import com.codecool.virtualstylist.wardrobe.Cloth;
-import com.codecool.virtualstylist.wardrobe.ClothesProperties;
-import com.codecool.virtualstylist.wardrobe.WardrobeDataAccess;
+import com.codecool.virtualstylist.user.User;
+import com.codecool.virtualstylist.user.UserDataAccess;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class DbInit implements CommandLineRunner {
+public class DbInitClothes implements CommandLineRunner {
 
     private UserDataAccess userRepository;
     private WardrobeDataAccess wardrobeDataAccess;
-    private PasswordEncoder passwordEncoder;
 
-    public DbInit(@Qualifier("userPostgresAccess") UserDataAccess userRepository, PasswordEncoder passwordEncoder, @Qualifier("wardrobeDataAccess") WardrobeDataAccess wardrobeDataAccess) {
+    public DbInitClothes(@Qualifier("userPostgresAccess") UserDataAccess userRepository, @Qualifier("wardrobeDataAccess") WardrobeDataAccess wardrobeDataAccess) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
         this.wardrobeDataAccess = wardrobeDataAccess;
     }
 
     @Override
     public void run(String... args) {
-
-        User adam = new User("adam@adam.pl", passwordEncoder.encode("adam123"), "adam", Gender.MALE);
+        User adam = userRepository.findAll().get(0);
         Cloth cloth1 = new Cloth("xd", ClothesProperties.BodyPart.CHEST, ClothesProperties.ClothType.JEANS, ClothesProperties.Color.RED,"xd","xd", ClothesProperties.Style.BOHO,"xd", ClothesProperties.Size.S,true,"xd",adam);
         Cloth cloth2 = new Cloth("dx", ClothesProperties.BodyPart.CHEST, ClothesProperties.ClothType.JEANS, ClothesProperties.Color.RED,"xd","xd", ClothesProperties.Style.BOHO,"xd", ClothesProperties.Size.S,true,"xd",adam);
         this.userRepository.save(adam);
