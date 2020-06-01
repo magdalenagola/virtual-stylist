fbb1e1547af6fc4562d9f3e2fb1d06a66a228ac3
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-01 17:57:39 +0200

    Add Bean annotation

--- src/main/java/com/codecool/virtualstylist/configuration/WebMvcConfig.java
+++ src/main/java/com/codecool/virtualstylist/configuration/WebMvcConfig.java
@@ -1,11 +1,13 @@
 package com.codecool.virtualstylist.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig {
+    @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
