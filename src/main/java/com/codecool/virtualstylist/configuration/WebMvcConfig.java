c16fed4fc902f9aab653676470f3005f283a090d
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-01 17:50:49 +0200

    Add WebMvcConfigurer

---
+++ src/main/java/com/codecool/virtualstylist/configuration/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package com.codecool.virtualstylist.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+}
