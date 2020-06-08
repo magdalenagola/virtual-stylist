e28db58d974e275141bb287a3aa0ed1595fc720e
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-08 14:33:49 +0200

    Import JpaRepository

--- src/main/java/com/codecool/virtualstylist/user/RoleDataAccess.java
+++ src/main/java/com/codecool/virtualstylist/user/RoleDataAccess.java
@@ -1,5 +1,6 @@
 package com.codecool.virtualstylist.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
