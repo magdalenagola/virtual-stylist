cc6a1375d0c23f59baf0e6217cd20d6680c72f6c
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-08 16:11:50 +0200

    Add user for registration DTO

---
+++ src/main/java/com/codecool/virtualstylist/user/UserForRegistrationDTO.java
@@ -0,0 +1,76 @@
+package com.codecool.virtualstylist.user;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Set;
+
+
+public class UserForRegistrationDTO {
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("password")
+    private String password;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("gender")
+    private String gender;
+
+    @JsonProperty("roles")
+    private Set<Role> roles;
+
+    public UserForRegistrationDTO() {
+    }
+
+    public UserForRegistrationDTO(String email, String password, String name, String gender, Set<Role> roles) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.gender = gender;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
