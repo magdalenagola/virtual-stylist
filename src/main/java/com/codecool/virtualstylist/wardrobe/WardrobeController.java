b204d9e57ab6fd16a75db177c0bb1a881e974fac
szczepanTopolski <szczepanPL678@gmail.com>
2020-06-09 15:10:17 +0200

    Add retrieve user from jwt token and pass his id to methods

--- src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeController.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/WardrobeController.java
@@ -1,5 +1,6 @@
 package com.codecool.virtualstylist.wardrobe;
 
+import com.codecool.virtualstylist.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,12 +14,12 @@ import org.springframework.web.bind.annotation.*;
 public class WardrobeController {
 
     private WardrobeService wardrobeService;
-   // private UserService userService;
+    private AuthService authService;
 
     @Autowired
-    public WardrobeController(WardrobeService wardrobeService){//, UserService userService) {
+    public WardrobeController(WardrobeService wardrobeService, AuthService authService){
         this.wardrobeService = wardrobeService;
-       // this.userService = userService;
+        this.authService = authService;
     }
 
     @GetMapping
@@ -27,31 +28,35 @@ public class WardrobeController {
             sort = "id",
             direction = Sort.Direction.ASC
     ) Pageable pageable){
-        return ResponseEntity.ok(wardrobeService.getAllClothesByUserId(1, pageable));
+        User user = authService.findUserByEmail();
+        return ResponseEntity.ok(wardrobeService.getAllClothesByUserId(user.getId(), pageable));
     }
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<ClothForDisplayDTO> getCloth(@PathVariable("id") Integer id){
-        return ResponseEntity.ok(wardrobeService.getClothById(id));
+    public ResponseEntity<ClothForDisplayDTO> getCloth(@PathVariable("id") int id){
+        User user = authService.findUserByEmail();
+        return ResponseEntity.ok(wardrobeService.getClothById(id, user.getId()));
     }
 
     @PutMapping
     public ResponseEntity editCloth(@RequestBody ClothForUpdateDTO cloth){
-        wardrobeService.editCloth(cloth);
+        User user = authService.findUserByEmail();
+        wardrobeService.editCloth(cloth, user);
         return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity deleteCloth(@PathVariable("id") Integer id){
-        //TODO findUser
-        wardrobeService.deleteCloth(id, 1);
+    public ResponseEntity deleteCloth(@PathVariable("id") int id){
+        User user = authService.findUserByEmail();
+        wardrobeService.deleteCloth(id, user.getId());
         return ResponseEntity.noContent().build();
     }
 
     @PostMapping
     public ResponseEntity addCloth(@RequestBody ClothForCreationDTO clothForCreation){
-        wardrobeService.addCloth(clothForCreation);
+        User user = authService.findUserByEmail();
+        wardrobeService.addCloth(clothForCreation, user);
         return ResponseEntity.noContent().build();
     }
 }
