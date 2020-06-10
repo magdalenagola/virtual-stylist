f003e75762e08b5b61e2d44947f70d5106279cf0
szczepanTopolski <szczepanPL678@gmail.com>
2020-06-10 19:03:34 +0200

    Add findUserById method

--- src/main/java/com/codecool/virtualstylist/user/UserDataAccess.java
+++ src/main/java/com/codecool/virtualstylist/user/UserDataAccess.java
@@ -10,4 +10,5 @@ public interface UserDataAccess extends JpaRepository<User,Integer> {
 
     Optional<User> findUserByEmail(String email);
     Boolean existsByEmail(String email);
+    Optional<User> findUserById(int id);
 }
