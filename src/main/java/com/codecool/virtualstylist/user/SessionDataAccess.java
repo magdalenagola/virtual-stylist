e43f087932ba8e52d30cd0f35d55e12d69b77017
szczepanTopolski <szczepanPL678@gmail.com>
2020-06-10 12:07:56 +0200

    Remove session related classes

--- src/main/java/com/codecool/virtualstylist/user/SessionDataAccess.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.virtualstylist.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository("sessionPostgresAccess")
-interface SessionDataAccess extends JpaRepository<Session,Integer> {
-}
