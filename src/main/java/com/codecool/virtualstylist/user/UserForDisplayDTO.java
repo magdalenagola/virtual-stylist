2d5eb8874225b9fec2fc0d14f8c1138df013c0a3
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-11 10:12:08 +0200

    Add user for display DTO

---
+++ src/main/java/com/codecool/virtualstylist/user/UserForDisplayDTO.java
@@ -0,0 +1,33 @@
+package com.codecool.virtualstylist.user;
+
+
+public class UserForDisplayDTO {
+
+    private String email;
+    private String name;
+    private Gender gender;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+}
