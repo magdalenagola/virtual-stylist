3c41de75fed24b83d2a740bd29838208321ba0ae
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-12 13:43:40 +0200

    Add getters and setters

--- src/main/java/com/codecool/virtualstylist/stylization/StylizationForCreationDTO.java
+++ src/main/java/com/codecool/virtualstylist/stylization/StylizationForCreationDTO.java
@@ -13,4 +13,20 @@ public class StylizationForCreationDTO {
 
     @JsonProperty("tag")
     private String tag;
+
+    public List<ClothForDisplayWardrobeDTO> getClothes() {
+        return clothes;
+    }
+
+    public void setClothes(List<ClothForDisplayWardrobeDTO> clothes) {
+        this.clothes = clothes;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
