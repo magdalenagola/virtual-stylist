4e3f934fbd1e0ba6ccf51db59f0808d645c5148a
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-12 13:02:12 +0200

    Add post mapping for new stylization creation

--- src/main/java/com/codecool/virtualstylist/stylization/StylizationController.java
+++ src/main/java/com/codecool/virtualstylist/stylization/StylizationController.java
@@ -1,6 +1,10 @@
 package com.codecool.virtualstylist.stylization;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,11 @@ public class StylizationController {
     @Autowired
     public StylizationController(StylizationService stylizationService) {
         this.stylizationService = stylizationService;
+    }
+
+    @PostMapping
+    ResponseEntity adddStylization(@RequestBody StylizationForCreationDTO stylizationForCreation){
+        stylizationService.addStylization(stylizationForCreation);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
