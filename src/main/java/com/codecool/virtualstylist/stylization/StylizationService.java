995a7a9c071960d4f5f1a7b12037753aa5213ead
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-12 13:45:02 +0200

    Save stylization to database

--- src/main/java/com/codecool/virtualstylist/stylization/StylizationService.java
+++ src/main/java/com/codecool/virtualstylist/stylization/StylizationService.java
@@ -1,15 +1,32 @@
 package com.codecool.virtualstylist.stylization;
 
+import com.codecool.virtualstylist.user.User;
+import com.codecool.virtualstylist.wardrobe.Cloth;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StylizationService {
 
     private final StylizationDataAccess stylizationDataAccess;
+    private final ModelMapper modelMapper;
 
     @Autowired
-    public StylizationService(StylizationDataAccess stylizationDataAccess) {
+    public StylizationService(StylizationDataAccess stylizationDataAccess, ModelMapper modelMapper) {
         this.stylizationDataAccess = stylizationDataAccess;
+        this.modelMapper = modelMapper;
+    }
+
+    public void addStylization(User user, StylizationForCreationDTO stylizationForCreation) {
+        List<Cloth> clothes = stylizationForCreation.getClothes().stream()
+                .map(clothForDisplay->modelMapper.map(clothForDisplay, Cloth.class))
+                .collect(Collectors.toList());
+
+        Stylization stylization = new Stylization(clothes, stylizationForCreation.getTag(), user);
+        stylizationDataAccess.save(stylization);
     }
 }
