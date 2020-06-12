07aad17becb035348e4ffa3dfff5c6d69b5f7863
szczepanTopolski <szczepanPL678@gmail.com>
2020-06-12 16:01:34 +0200

    Add get all stylizations by cloth id

--- src/main/java/com/codecool/virtualstylist/stylization/StylizationController.java
+++ src/main/java/com/codecool/virtualstylist/stylization/StylizationController.java
@@ -2,19 +2,18 @@ package com.codecool.virtualstylist.stylization;
 
 import com.codecool.virtualstylist.user.AuthService;
 import com.codecool.virtualstylist.user.User;
+import com.codecool.virtualstylist.wardrobe.ClothForDisplayStylizationDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/stylization")
@@ -44,5 +43,11 @@ public class StylizationController {
     ) Pageable pageable){
         User user = authService.findUserByEmail();
         return ResponseEntity.ok(stylizationService.getAllStylizations(pageable, user.getId()));
+    }
+
+    @GetMapping("/{clothId}")
+    public ResponseEntity<List<ClothForDisplayStylizationDTO>> getAllStylizationsByClothId(@PathVariable("clothId") int clothId){
+        User user = authService.findUserByEmail();
+        return ResponseEntity.ok(stylizationService.getAllStylizationsByClothId(clothId,user.getId()));
     }
 }
