c915ed494677e4f14ae6e698b27013944c1320c1
szczepanTopolski <szczepanPL678@gmail.com>
2020-06-15 15:49:07 +0200

    Make user data access extend pagind and sorting repository

--- src/main/java/com/codecool/virtualstylist/user/UserDataAccess.java
+++ src/main/java/com/codecool/virtualstylist/user/UserDataAccess.java
@@ -2,13 +2,13 @@ package com.codecool.virtualstylist.user;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository("userPostgresAccess")
-interface UserDataAccess extends JpaRepository<User,Integer> {
+interface UserDataAccess extends PagingAndSortingRepository<User,Integer> {
 
     Optional<User> findUserByEmail(String email);
     Boolean existsByEmail(String email);
