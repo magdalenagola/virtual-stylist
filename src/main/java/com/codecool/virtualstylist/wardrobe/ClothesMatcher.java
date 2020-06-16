2a806aa920d3e1fbc2bed30b7a49566725b896da
Magdalena Gola <turcza.magdalena@gmail.com>
2020-06-16 14:22:23 +0200

    Find matching clothes by style

--- src/main/java/com/codecool/virtualstylist/wardrobe/ClothesMatcher.java
+++ src/main/java/com/codecool/virtualstylist/wardrobe/ClothesMatcher.java
@@ -1,9 +1,13 @@
 package com.codecool.virtualstylist.wardrobe;
 
+import java.util.List;
+import java.util.Map;
 import java.util.function.BiFunction;
 
 
 public interface ClothesMatcher extends BiFunction<Cloth, Cloth, Boolean> {
+
+    Map<ClothesProperties.Style, List<ClothesProperties.Style>> matchingStyles = ClothesProperties.createMatchingStylesMap();
 
     static ClothesMatcher isPatternMatching(){
         return (clothToBeMatched, cloth)->{
@@ -13,8 +17,12 @@ public interface ClothesMatcher extends BiFunction<Cloth, Cloth, Boolean> {
         };
     }
 
-    //TODO isStyleMatching
-    //TODO isColorMatching
+    static ClothesMatcher isStyleMatching(){
+        return (clothToBeMatched, cloth)->{
+            if(matchingStyles.get(clothToBeMatched.getStyle()).contains(cloth.getStyle())) return true;
+            return false;
+        };
+    }
 
     default ClothesMatcher and (ClothesMatcher other){
         return (clothToBeMatched, cloth) -> {
