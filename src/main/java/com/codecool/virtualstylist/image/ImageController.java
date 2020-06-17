8aea61949ccfc85a389fe8a7844246d08bb45114
szczepanTopolski <szczepanPL678@gmail.com>
2020-06-17 16:43:51 +0200

    Adjust controller's method to upload file

--- src/main/java/com/codecool/virtualstylist/image/ImageController.java
+++ src/main/java/com/codecool/virtualstylist/image/ImageController.java
@@ -1,7 +1,10 @@
 package com.codecool.virtualstylist.image;
 
 import com.codecool.virtualstylist.exception.ResourceNotFoundException;
+import com.codecool.virtualstylist.user.AuthService;
+import com.codecool.virtualstylist.user.User;
 import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,14 +12,21 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/img")
 class ImageController {
+
+    private final ImageService imageService;
+    private final AuthService authService;
+
+    @Autowired
+    ImageController(ImageService imageService, AuthService authService) {
+        this.imageService = imageService;
+        this.authService = authService;
+    }
 
     @GetMapping(
             value = "/{fileName}",
@@ -33,26 +43,8 @@ class ImageController {
 
     @PostMapping
     Map<String, String> saveUploadedFile(@RequestParam(name = "file") MultipartFile multipartFile) {
-        if (!multipartFile.isEmpty()) {
-            try {
-                UUID uuid = UUID.randomUUID();
-                String filename = uuid.toString() + ".jpg";
-                byte[] bytes = multipartFile.getBytes();
-                File file = new File("src/main/resources",filename);
-                file.createNewFile();
-                BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(file));
-                stream.write(bytes);
-                stream.close();
-                Map<String, String> fileNameJson = new HashMap<>();
-                fileNameJson.put("fileName", filename);
-                return fileNameJson;
-            } catch (Exception e) {
-                throw new IllegalArgumentException();
-            }
-        } else {
-            System.out.println("File is empty");
-            throw new IllegalArgumentException();
-        }
+        User user = authService.findUserByEmail();
+        return imageService.uploadFile(user, multipartFile);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
